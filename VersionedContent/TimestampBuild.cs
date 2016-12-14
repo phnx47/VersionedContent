41b068938154e15091ab0fff5df8cbab930ec06b
Sergey Kuznetsov <sergey.kuznetsov@hotmail.com>
2016-12-15 00:55:16 +0300

    prototype

---
+++ VersionedContent/TimestampBuild.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace VersionedContent
+{
+    /// <summary>
+    /// Helper for get timestamp from calling dll
+    /// </summary>
+    public static class TimestampBuild
+    {
+        /// <summary>
+        /// Get Timestamp Build
+        /// </summary>
+        public static int Get()
+        {
+            var filePath = Assembly.GetCallingAssembly().Location;
+
+            /* for netstandart
+             *  var filePath = PlatformServices.Default.Application.ApplicationBasePath
+                + Path.DirectorySeparatorChar
+                + PlatformServices.Default.Application.ApplicationName + ".dll";
+             */
+
+            const int cPeHeaderOffset = 60;
+            const int cLinkerTimestampOffset = 8;
+            var b = new byte[2048];
+            using (Stream s = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                s.Read(b, 0, 2048);
+            }
+
+            var i = BitConverter.ToInt32(b, cPeHeaderOffset);
+            var secondsSince1970 = BitConverter.ToInt32(b, i + cLinkerTimestampOffset);
+            return secondsSince1970;
+        }
+    }
+}
---
+++ VersionedContent/UrlHelperExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Web.Caching;
+using System.Web.Mvc;
+
+namespace VersionedContent
+{
+    /// <summary>
+    /// Extensions for UrlHelper
+    /// </summary>
+    public static class UrlHelperExtensions
+    {
+        /// <summary>
+        /// Versioned content
+        /// </summary>
+        public static string VersionedContent(this UrlHelper helper, string contentPath)
+        {
+            var context = helper.RequestContext.HttpContext;
+            if (context.Cache[contentPath] != null)
+                return context.Cache[contentPath] as string;
+
+            var physicalPath = context.Server.MapPath(contentPath);
+            var version = "?v=" + TimestampBuild.Get();
+            var translatedContentPath = helper.Content(contentPath);
+            var versionedContentPath = translatedContentPath + version;
+            context.Cache.Add(physicalPath, version, null, DateTime.UtcNow.AddDays(1), TimeSpan.Zero, CacheItemPriority.Normal, null);
+            context.Cache[contentPath] = versionedContentPath;
+            return versionedContentPath;
+        }
+    }
+}
