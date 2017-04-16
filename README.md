446ec5843abcf8c191b330ade751cb250ab6ed8c
Sergey Kuznetsov <sergey.kuznetsov@hotmail.com>
2017-04-16 20:03:32 +0300

    Create samples

---
+++ README.md
@@ -0,0 +1,37 @@
+# VersionedContent
+
+[![NuGet](https://img.shields.io/nuget/v/VersionedContent.svg)](https://www.nuget.org/packages/VersionedContent) [![License MIT](https://img.shields.io/badge/license-MIT-green.svg)](https://opensource.org/licenses/MIT) 
+
+Use the current timestamp as the version for static files. Cached for one day
+
+```
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test Page</title>
+</head>
+<body>
+<div>
+    Content
+</div>
+    <script src="@Url.VersionedContent("~/Scripts/main.js")"></script>
+</body>
+</html>
+```
+
+Result:
+
+```
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test Page</title>
+</head>
+<body>
+<div>
+    Content
+</div>
+    <script src="/Scripts/main.js?v=1492371642"></script>
+</body>
+</html>
+```
--- VersionedContent/UrlHelperExtensions.cs
+++ VersionedContent/UrlHelperExtensions.cs
@@ -1,16 +1,18 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 using System.Web.Caching;
 using System.Web.Mvc;
 
 namespace VersionedContent
 {
     /// <summary>
-    /// Extensions for UrlHelper
+    ///     Extensions for UrlHelper
     /// </summary>
     public static class UrlHelperExtensions
     {
         /// <summary>
-        /// Versioned content
+        ///     Versioned content
         /// </summary>
         public static string VersionedContent(this UrlHelper helper, string contentPath)
         {
@@ -18,11 +20,12 @@ namespace VersionedContent
             if (context.Cache[contentPath] != null)
                 return context.Cache[contentPath] as string;
 
+            var now = DateTime.UtcNow;
             var physicalPath = context.Server.MapPath(contentPath);
-            var version = "?v=" + TimestampBuild.Get();
+            var version = "?v=" + (long)(now - new DateTime(1970, 1, 1, 0, 0, 0, 0).ToUniversalTime()).TotalSeconds;
             var translatedContentPath = helper.Content(contentPath);
             var versionedContentPath = translatedContentPath + version;
-            context.Cache.Add(physicalPath, version, null, DateTime.UtcNow.AddDays(1), TimeSpan.Zero, CacheItemPriority.Normal, null);
+            context.Cache.Add(physicalPath, version, null, now.AddDays(1), TimeSpan.Zero, CacheItemPriority.Normal, null);
             context.Cache[contentPath] = versionedContentPath;
             return versionedContentPath;
         }
